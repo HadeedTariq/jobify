d480160ad6214eac8fbe83e66710cdc062d97f45
HadeedTariq <computeranalog351@gmail.com>
2024-10-21 21:19:01 +0500

    create simple pdf viewer

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -4,6 +4,15 @@ import UploadResume from "@/components/UploadResume";
 import { pool } from "@/lib/db";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { v2 as cloudinary } from "cloudinary";
+import Link from "next/link";
+import PdfViewer from "@/components/PdfViewer";
+
+cloudinary.config({
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "lmsproject",
+});
 
 const AdditionalInfo = async () => {
   const user = await currentUser();
@@ -23,8 +32,24 @@ const AdditionalInfo = async () => {
   const registerUser = async (formData: FormData) => {
     "use server";
     const passion = formData.get("passion");
-    const resume = formData.get("resume");
-    console.log(resume);
+    const resume = formData.get("resume") as File;
+    const fileBuffer = await resume.arrayBuffer();
+    const buffer = Buffer.from(fileBuffer);
+    const result = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { resource_type: "auto", format: "pdf" },
+          function (error, result) {
+            if (error) {
+              reject(error);
+              return;
+            }
+            resolve(result);
+          }
+        )
+        .end(buffer);
+    });
+    console.log(result);
   };
   return (
     <div className="flex flex-col">
@@ -32,6 +57,7 @@ const AdditionalInfo = async () => {
         <PassionSelector />
         <UploadResume />
         <Button type="submit">Update</Button>
+        <PdfViewer />
       </form>
     </div>
   );
---
+++ src/components/PdfViewer.tsx
@@ -0,0 +1,7 @@
+const PdfViewer = () => {
+  return (
+    <iframe src="https://res.cloudinary.com/lmsproject/image/upload/v1729426736/pg8htngxl6urt6qqaybb.pdf"></iframe>
+  );
+};
+
+export default PdfViewer;
